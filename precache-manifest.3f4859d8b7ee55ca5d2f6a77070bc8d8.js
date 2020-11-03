31f2b7ad06b08364e498d1f35aead77bcf77dc59
dhruvchawla102 <dhruvchawla102@gmail.com>
2020-11-03 10:40:54 +0530

    Updates

---
+++ precache-manifest.3f4859d8b7ee55ca5d2f6a77070bc8d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f5059acb243b2acf9066eed8fd4850",
+    "url": "/ComplaintSection/index.html"
+  },
+  {
+    "revision": "bab02a2379a0153b79c0",
+    "url": "/ComplaintSection/static/css/main.d6f92e6e.chunk.css"
+  },
+  {
+    "revision": "9f84fb29d6307fe4e433",
+    "url": "/ComplaintSection/static/js/2.1ccc3800.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/ComplaintSection/static/js/2.1ccc3800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bab02a2379a0153b79c0",
+    "url": "/ComplaintSection/static/js/main.e2061418.chunk.js"
+  },
+  {
+    "revision": "cea08009a307d8fcc54c",
+    "url": "/ComplaintSection/static/js/runtime-main.db2da429.js"
+  }
+]);
